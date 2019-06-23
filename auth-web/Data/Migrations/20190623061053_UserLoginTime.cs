a231d2ceb3bc85d8a8b997f6d6b0f4b58e30f7af
Matt Cosand <matt@cosand.net>
2019-06-22 23:37:51 -0700

    Track last login time for user accounts

---
+++ auth-web/Data/Migrations/20190623061053_UserLoginTime.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SarData.Auth.Data.Migrations
+{
+    public partial class UserLoginTime : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "LastLogin",
+                schema: "auth",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LastLogin",
+                schema: "auth",
+                table: "AspNetUsers");
+        }
+    }
+}
